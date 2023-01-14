e9d619c768806815ff79d0d123374e6e56b0369a
Satheesh D S <davidsonsatheesh@gmail.com>
2023-01-14 23:04:09 +0530

    sample api to deploy

---
+++ cmd/order_processor/api_default.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func CancelOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func CreateOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func GetOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
--- cmd/order_processor/main.go
+++ cmd/order_processor/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"strings"
+
+	"github.com/satheeshds/sellerapp/pkg/common"
 )
 
 func main() {
 	log.Printf("Server started!")
+
+	router := common.NewRouter(routes)
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
+
+var routes = common.Routes{
+
+	common.Route{
+		Name:        "CancelOrder",
+		Method:      strings.ToUpper("Patch"),
+		Pattern:     "/v1/order/{id}",
+		HandlerFunc: CancelOrder,
+	},
+
+	common.Route{
+		Name:        "CreateOrder",
+		Method:      strings.ToUpper("Post"),
+		Pattern:     "/v1/order",
+		HandlerFunc: CreateOrder,
+	},
+
+	common.Route{
+		Name:        "GetOrder",
+		Method:      strings.ToUpper("Get"),
+		Pattern:     "/v1/order/{id}",
+		HandlerFunc: GetOrder,
+	},
 }
